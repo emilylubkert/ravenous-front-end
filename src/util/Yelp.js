111656049be60e5b3fd92a10e7b0fdc734edcb6b
emilylubkert <93680915+emilylubkert@users.noreply.github.com>
2022-07-03 14:31:04 -0400

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey =
+  'rEDX9ogTU2DCV5b3GEpAo9gEVxo5uTXgguSHOz5zllsFpAcrs7G2Iud0Pysb6juzaAULtnykYIM4uXG_C7RaEub9J1yQ-5egmg96vS-vrDFg39_3Li7gWp--n13AYnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
